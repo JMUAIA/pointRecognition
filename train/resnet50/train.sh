7baa3dc3c4709a819d01fbc20bf81ad8f574cde8
JMUAIA <JMUAIASSOCIATION@163.com>
2018-04-06 22:38:25 +0800

    mode 180406 by js

--- train/resnet50/train.sh
+++ train/resnet50/train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 LOG=./log/train-origin-`date +%Y-%m-%d-%H-%M-%S`.log
-~/software/caffe/build/tools/caffe train -solver solver.prototxt -model train_val.prototxt -gpu 0,1 2>&1 | tee $LOG
+~/software/caffe/build/tools/caffe train -solver solver.prototxt -model resnet_train_val.prototxt -gpu 0 2>&1 | tee $LOG
